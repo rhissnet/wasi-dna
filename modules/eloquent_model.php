d15cae571a45d18beec75be907bb87d8f46e491a
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2016-09-15 16:45:41 -0500

    Update

--- modules/eloquent_model.php
+++ modules/eloquent_model.php
@@ -14,10 +14,10 @@ $capsule->addConnection([
     'database'  => $config->data['dbname'],
     'username'  => $config->data['dbuser'],
     'password'  => $config->data['dbpass'],
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    //'charset'   => 'latin1',
-    //'collation' => null,
+    //'charset'   => 'utf8',
+    //'collation' => 'utf8_unicode_ci',
+    'charset'   => 'latin1',
+    'collation' => null,
     'prefix'    => '',
 ]);
 
